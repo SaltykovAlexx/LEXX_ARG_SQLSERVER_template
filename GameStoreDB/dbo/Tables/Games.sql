fd1215e2354d105990f5fe43a5cda3429337211e
SaltykovAlexx <49457364+SaltykovAlexx@users.noreply.github.com>
2019-06-14 11:03:16 +0300

    First

---
+++ GameStoreDB/dbo/Tables/Games.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Games](
+	[GameId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[Category] [nvarchar](max) NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[ImageData] [varbinary](max) NULL,
+	[ImageMimeType] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.Games] PRIMARY KEY CLUSTERED 
+(
+	[GameId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
